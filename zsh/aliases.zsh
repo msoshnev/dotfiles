532675257c512ccb9586eba759ed8269a18861d2
Maxim Soshnev <m.soshnev@yadro.com>
2023-09-11 21:09:34 +0300

    Add zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias s='sudo'
+alias p='python'
+alias p2='python2'
+alias p3='python3'
+alias vim='nvim'
+
+alias gpom='git pull origin master'
+
+alias rmpycache="find . -type d -name '__pycache__' -exec rm -v -rf {} ';'"
+
