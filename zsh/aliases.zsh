3f654f170b01e37dfb3bf0469ce457813ea0e490
Maxim Soshnev <m.soshnev@yadro.com>
2025-01-30 18:49:43 +0300

    commit

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,8 +3,10 @@ alias p='python'
 alias p2='python2'
 alias p3='python3'
 alias vim='nvim'
+alias v='nvim'
 
 alias gpom='git pull origin master'
+alias gpo='git pull origin'
+alias gp='git push origin $(git branch --show-current)'
 
 alias rmpycache="find . -type d -name '__pycache__' -exec rm -v -rf {} ';'"
-
